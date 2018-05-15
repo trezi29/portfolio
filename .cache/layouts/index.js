7f95df5613578151595f75b206412fa3a6542335
Matteo Tressi <m.tressi@gmail.com>
2018-05-15 16:31:42 +0200

    add gastby starter

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/mc01387/Documents/GitHub/portfolio/src/layouts/index.js"
+  import data from "/Users/mc01387/Documents/GitHub/portfolio/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
